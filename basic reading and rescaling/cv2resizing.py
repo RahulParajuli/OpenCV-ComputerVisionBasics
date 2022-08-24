b88ab69788df71dee38536a0c47e3c1411fe32c3
RahulParajuli <sr2rahul19@gmail.com>
2022-08-24 13:36:48 +0545

    added resizing

--- basic reading and rescaling/cv2resizing.py
+++ basic reading and rescaling/cv2resizing.py
@@ -1,6 +1,4 @@
 import cv2 as cv
-img = cv.imread("resources/images/dog.jpg")
-cv.imshow("dog",img)
 
 def rescaleFrame(frame, scale=0.75):
   width = int(frame.shape[1] * scale)
@@ -8,14 +6,16 @@ def rescaleFrame(frame, scale=0.75):
   dimentions = (width, height)
   return cv.resize(frame, dimentions, interpolation=cv.INTER_AREA)
 
-capture = cv.VideoCapture("resources/videos/nature.MOV") # 0 is the id of the camera
+capture = cv.VideoCapture("C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/videos/nature.MOV") # 0 is the id of the camera
 
-# capture = cv.VideoCapture("resources/videos/nature.MOV") # 0 is the id of the camera
 while True:
-  ret, frame = capture.read()
+  isTrue, frame = capture.read()
   frame_resized = rescaleFrame(frame)
 
-  cv.imshow("video", capture)
+  cv.imshow("video", frame)
   cv.imshow("resized",frame_resized)
-  if cv.waitKey(1) & 0xFF == ord('q'):
+  if cv.waitKey(20) & 0xFF == ord('q'):
     break
+
+capture.release()
+cv.destroyAllWindows()
