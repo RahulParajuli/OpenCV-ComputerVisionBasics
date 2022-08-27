4aa5ebd00622311d3b46e5a7d5a9228b75660c56
RahulParajuli <sr2rahul19@gmail.com>
2022-08-27 14:08:53 +0545

    lab modified

--- Color Spaces/LAB.py
+++ Color Spaces/LAB.py
@@ -1,6 +1,7 @@
 import cv2
 
-img = cv2.imread("C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/images/dog2.jpg")
+img = cv2.imread(
+    "C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/images/dog2.jpg")
 cv2.imshow("original", img)
 
 labs = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
