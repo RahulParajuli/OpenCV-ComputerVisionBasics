464d918318231273acf39df929aa4c3768c3dc4c
RahulParajuli <sr2rahul19@gmail.com>
2022-08-30 14:26:34 +0545

    adaptive thresholding

---
+++ Thresholding/adaptiveThresh.py
@@ -0,0 +1,16 @@
+import cv2
+img = cv2.imread("/home/drox/Documents/computervision/ComputerVisionExercise/resources/images/dog3.jpg")
+
+cv2.imshow("Original", img)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Adaptive thresholding
+
+thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+
+cv2.imshow("Adaptive Mean Thresholded", thresh)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
