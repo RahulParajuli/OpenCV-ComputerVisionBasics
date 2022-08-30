dbb6e331742bc382f35716666da74d640ed07ead
RahulParajuli <sr2rahul19@gmail.com>
2022-08-30 14:34:53 +0545

    Adaptive gaussian threshold

--- Thresholding/adaptiveThresh.py
+++ Thresholding/adaptiveThresh.py
@@ -7,9 +7,13 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Adaptive thresholding
 
-thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 9)
 
 cv2.imshow("Adaptive Mean Thresholded", thresh)
+
+gaussianthresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 9)
+
+cv2.imshow("Adaptive Gaussian Thresholded", gaussianthresh)
 
 cv2.waitKey(0)
 
