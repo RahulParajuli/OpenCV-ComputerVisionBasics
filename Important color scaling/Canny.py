165f7d2be16d2383221f101c8a8cda801d6062ce
RahulParajuli <sr2rahul19@gmail.com>
2022-08-31 11:34:49 +0545

    modified and added Sobel

--- Important color scaling/Canny.py
+++ Important color scaling/Canny.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 
-img = cv.imread("C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/images/rnr1.jpg")
+img = cv.imread("/home/drox/Documents/computervision/ComputerVisionExercise/resources/images/rnr1.jpg")
 canny = cv.Canny(img, 100, 200)
 cv.imshow("canny", canny)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 #with blur and canny tor reduce some of the edges
-img = cv.imread("C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/images/rnr1.jpg")
+img = cv.imread("/home/drox/Documents/computervision/ComputerVisionExercise/resources/images/rnr1.jpg")
 blur= cv.GaussianBlur(img,(7,7), cv.BORDER_DEFAULT)
 canny = cv.Canny(blur, 100,200)
 cv.imshow("Canny Edges", canny)
--- Important color scaling/gaussianBlur.py
+++ Important color scaling/gaussianBlur.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-img = cv.imread("C:/Users/batman/Desktop/mypractices/computer vision/ComputerVisionExercise/resources/images/rnr1.jpg")
+img = cv.imread("/home/drox/Documents/computervision/ComputerVisionExercise/resources/images/rnr1.jpg")
 blur = cv.GaussianBlur(img,(7,7), cv.BORDER_DEFAULT)
 cv.imshow("blur", blur)
 cv.waitKey(0)
