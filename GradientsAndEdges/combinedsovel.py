8da3737447f38f9136c47984cfd8a6e22611bb93
RahulParajuli <sr2rahul19@gmail.com>
2022-09-01 10:45:22 +0545

    combined sobel

---
+++ GradientsAndEdges/combinedsovel.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+img  = cv2.imread("/home/drox/Documents/computervision/ComputerVisionExercise/resources/images/dog2.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("gray", gray)
+
+lap = cv2.Laplacian(gray, cv2.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv2.imshow("lap", lap)
+
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+
+combined_sobel = cv2.bitwise_or(sobelx, sobely)
+
+cv2.imshow("sobelx", sobelx)
+cv2.imshow("sobely", sobely)
+cv2.imshow("combined_sobel", combined_sobel)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
